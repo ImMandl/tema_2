3f492013e6382fe9c3a2544cdf5bfc70bf09658e
Amund Bjoernstad <amund.bjornstad@outlook.com>
2019-10-04 09:54:58 +0200

    øvelser

---
+++ 2019.10.04_dag6/filmer/index.js
@@ -0,0 +1,16 @@
+const sectionFilmer = document.querySelector("#sectionFilmer");
+const inpSearch = document.querySelector("#inpSearch");
+const btnSkrekk = document.querySelector("#btnSkrekk");
+const btnDrama = document.querySelector("#btnDrama");
+const btnHumor = document.querySelector("#btnHumor");
+
+const filmer = [
+    {tittel: "Sharknado", director: "Stanley Kubrick", genre: "Skrekk"},
+    {tittel: "Sharknado", director: "Stanley Kubrick", genre: "Skrekk"},
+    {tittel: "Love Story", director: "Steven Spielberg", genre: "Drama"},
+    {tittel: "The Long day", director: "Steven Spielberg", genre: "Drama"},
+    {tittel: "Alkymisten", director: "Erik Poppe", genre: "Humor"},
+    {tittel: "Wizard of Oz", director: "Erik Poppe", genre: "Humor"},
+    {tittel: "Fleksnes", director: "Brad Pitt", genre: "Humor"}
+];
+
